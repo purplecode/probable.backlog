f485707f1edd2e5df815a9257989f007cbb6a4aa
purplecode <niespammnie@gmail.com>
2014-08-30 22:22:27 +0200

    tiny cleaning

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -90,21 +90,3 @@ App.ModalDialogComponent = Ember.Component.extend({
         }
     }
 });
-
-var showdown = new Showdown.converter();
-
-Ember.Handlebars.helper('format-markdown', function(input) {
-    return new Handlebars.SafeString(showdown.makeHtml(input || ""));
-});
-
-Ember.Handlebars.helper('format-date', function(date) {
-    return date ? moment(date).fromNow() : 'N/A';
-});
-
-Ember.Handlebars.helper('format-displayname', function(name) {
-    return name.replace(/\(.*\)/, '');
-});
-
-Ember.Handlebars.helper('format-percent', function(number) {
-    return (Math.round(number * 100) / 100).toFixed(2) + '%';
-});
---
+++ app/static/js/helpers.js
@@ -0,0 +1,17 @@
+
+var showdown = new Showdown.converter();
+Ember.Handlebars.helper('format-markdown', function(input) {
+    return new Handlebars.SafeString(showdown.makeHtml(input || ""));
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+    return date ? moment(date).fromNow() : 'N/A';
+});
+
+Ember.Handlebars.helper('format-displayname', function(name) {
+    return name.replace(/\(.*\)/, '');
+});
+
+Ember.Handlebars.helper('format-percent', function(number) {
+    return (Math.round(number * 100) / 100).toFixed(2) + '%';
+});
