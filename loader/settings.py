f2954a9b883a55bd3943260267118a726a63d689
purplecode <niespammnie@gmail.com>
2014-08-31 00:30:48 +0200

    cc as project

--- loader/settings.py
+++ loader/settings.py
@@ -6,7 +6,7 @@ settings = {
         'project' : 'MCMP',
         'stories' : 'project = MINT AND (labels != idea OR labels is empty) and status != Closed and issuetype = Story ORDER BY Rank ASC',
         'epics' : 'project = MINT AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
-        'tasks' : 'project = MINT AND (issuetype = "Technical task" OR issuetype = Bug) AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+        'tasks' : 'project = MINT AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, Story, "Technical task")) AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
       }
     },
     'NDA' : {
@@ -16,19 +16,29 @@ settings = {
         'project' : 'NDA',
         'stories' : 'project = NDA and status != Closed and issuetype = Story ORDER BY Rank ASC',
         'epics' : 'project = NDA AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
-        'tasks' : 'project = NDA AND (issuetype = "Technical task" OR issuetype = Bug) AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+        'tasks' : 'project = NDA AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, Story, "Technical task")) AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
       }
     },
-    'RNTDIM' : {
-      'name' : 'RAN Dim',
-      'url' : 'https://jira3.inside.nsn.com',
-      'queries' : {
-        'project' : 'RNTDIM',
-        'stories' : 'project = "RAN Dim" and status != Closed and issuetype = "User Story" ORDER BY Rank ASC',
-        'epics' : 'project = "RAN Dim" AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
-        'tasks' : 'project = "RAN Dim" AND (issuetype != "Epic" AND issuetype != "Story" AND issuetype != "User story") AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
-      }
-    }
+    # 'RNTDIM' : {
+    #   'name' : 'RAN Dim',
+    #   'url' : 'https://jira3.inside.nsn.com',
+    #   'queries' : {
+    #     'project' : 'RNTDIM',
+    #     'stories' : 'project = "RAN Dim" and status != Closed and issuetype = "User Story" ORDER BY Rank ASC',
+    #     'epics' : 'project = "RAN Dim" AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+    #     'tasks' : 'project = "RAN Dim" AND (issuetype != "Epic" AND issuetype != "Story" AND issuetype != "User story") AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+    #   }
+    # },
+    # 'CSCLOUD ': {
+    #   'name' : 'Cloud Controller',
+    #   'url' : 'https://jira3.inside.nsn.com',
+    #   'queries' : {
+    #     'project' : 'CSCLOUD',
+    #     'stories' : 'project = CSCLOUD and status != Closed and issuetype = Story ORDER BY Rank ASC',
+    #     'epics' : 'project = CSCLOUD AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+    #     'tasks' : 'project = CSCLOUD AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, Story, "Technical task")) AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+    #   }
+    # }
   }
 
 
