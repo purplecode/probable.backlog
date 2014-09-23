b26fc23ba93bf986a4874e0c404a640eb0680faf
purplecode <niespammnie@gmail.com>
2014-09-23 22:50:21 +0200

    projects on navbar

--- backlog/static/js/app.js
+++ backlog/static/js/app.js
@@ -1,11 +1,6 @@
 App = Ember.Application.create({});
 
 App.Router.map(function() {
-    this.resource('charts', function() {
-        this.resource('project', {
-            path: ':project_id'
-        });
-    });
     this.resource('projects', function() {
         this.resource('project', {
             path: ':project_id'
