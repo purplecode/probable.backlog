8dd9cba544e4b08241ba9b1ccc710ce31135ac4e
purplecode <niespammnie@gmail.com>
2014-09-23 23:33:34 +0200

    selected link is green

--- backlog/static/libs/mozmonkey/eachIndexed.js
+++ backlog/static/libs/mozmonkey/eachIndexed.js
@@ -1,21 +1,3 @@
-
-var showdown = new Showdown.converter();
-Ember.Handlebars.helper('format-markdown', function(input) {
-    return new Handlebars.SafeString(showdown.makeHtml(input || ""));
-});
-
-Ember.Handlebars.helper('format-date', function(date) {
-    return date ? moment(date).fromNow() : 'N/A';
-});
-
-Ember.Handlebars.helper('format-displayname', function(name) {
-    return name.replace(/\(.*\)/, '');
-});
-
-Ember.Handlebars.helper('format-float', function(number) {
-    return (Math.round(number * 100) / 100).toFixed(2);
-});
-
 /**
   A replacement for #each that provides an index value (and other helpful values) for each iteration.
   Unless using `foo in bar` format, the item at each iteration will be accessible via the `item` variable.
