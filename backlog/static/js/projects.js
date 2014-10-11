5eb4d42031bf02b498c62f0722599f7539411e0b
purplecode <niespammnie@gmail.com>
2014-10-11 22:23:22 +0200

    search epics

--- backlog/static/js/projects.js
+++ backlog/static/js/projects.js
@@ -27,19 +27,28 @@ App.ProjectRoute = Ember.Route.extend({
     }
 });
 
+App.ProjectController = Ember.Controller.extend({
+    search: '',
+    filteredEpics: function() {
+        return _.filter(this.get('model.epics'), function(epic) {
+            return epic.summary.indexOf(this.search) > -1;
+        }, this);
+    }.property('model.epics', 'search')
+});
+
 var modalActions = function(scope) {
     return {
         openModal: function(modalName, model) {
             this.controllerFor(modalName).set('model', model);
             return this.render(modalName, {
-                into: 'project/'+scope,
+                into: 'project/' + scope,
                 outlet: 'modal'
             });
         },
         closeModal: function() {
             return this.disconnectOutlet({
                 outlet: 'modal',
-                parentView: 'project.'+scope
+                parentView: 'project.' + scope
             });
         }
     };
@@ -49,8 +58,9 @@ App.ProjectTableRoute = Ember.Route.extend({
     actions: modalActions('table')
 });
 
-App.ProjectChartsRoute = Ember.Route.extend({
-    actions: modalActions('charts')
+App.ProjectTableController = Ember.Controller.extend({
+    needs: "project",
+    filteredEpics: Ember.computed.alias('controllers.project.filteredEpics')
 });
 
 App.ProjectTableView = Ember.View.extend({
@@ -65,14 +75,21 @@ App.BacklogItemView = Ember.View.extend({
     labelClass: function() {
         var status = this.get('data').status;
         var classes = {
-            'In Progress' : 'label-success',
-            'Open' : 'label-info'
+            'In Progress': 'label-success',
+            'Open': 'label-info'
         };
         return classes[status] || 'label-default';
     }.property()
 });
 
-App.ProjectChartsController = Ember.Controller.extend({});
+App.ProjectChartsRoute = Ember.Route.extend({
+    actions: modalActions('charts')
+});
+
+App.ProjectChartsController = Ember.Controller.extend({
+    needs: "project",
+    filteredEpics: Ember.computed.alias('controllers.project.filteredEpics')
+});
 
 App.ProjectSummaryController = Ember.Controller.extend({});
 
@@ -88,7 +105,7 @@ App.ProgressBarGraph = Ember.View.extend({
         var $el = this.$()[0];
         var data = this.get('data');
         var history = data.history;
-        if(!_.isEmpty(history) && history.length > 1 && data.status != 'Open') {
+        if (!_.isEmpty(history) && history.length > 1 && data.status != 'Open') {
             var dueDate = data.dueDate;
             var chartData = new ProgressChartData(history, dueDate);
             var chart = new AreaChart($el, $($el).width());
@@ -109,7 +126,9 @@ App.SummaryGraph = Ember.View.extend({
     updateChart: function() {
         var $el = this.$()[0];
         var data = this.get('data');
-        var epics = _.filter(data.epics, function(epic) { return !_.isEmpty(epic.dueDate); });
+        var epics = _.filter(data.epics, function(epic) {
+            return !_.isEmpty(epic.dueDate);
+        });
         var chartData = new SummaryChartData(epics);
         var chart = new AreaChart($el, $($el).width(), 600);
         chart.draw(chartData);
