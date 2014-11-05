dc8246a267a1583fa143fc70c47c73fc7af024b9
purplecode <niespammnie@gmail.com>
2014-11-05 11:27:26 +0100

    django supervisor

--- backlog/config/settings.py
+++ backlog/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ember',
-    'backlog'
+    'backlog',
+    'django-supervisor.djsupervisor'
 )
 
 MIDDLEWARE_CLASSES = (
