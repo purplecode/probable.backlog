9c558c431a058bc3e87f48747807540689ef8856
purplecode <niespammnie@gmail.com>
2015-03-09 11:22:18 +0100

    cc removed from synchro

--- loader/settings.py
+++ loader/settings.py
@@ -9,6 +9,16 @@ settings = {
         'tasks' : 'project = MINT AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, "Technical task")) AND parent in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
       }
     },
+    'MAPS' : {
+      'name' : 'MINT Maps',
+      'url' : 'https://jira3.inside.nsn.com',
+      'queries' : {
+        'project' : 'MAPS',
+        'epics' : 'project = MAPS AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+        'stories' : 'project = MAPS and issuetype = Story and "Epic Link" in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+        'tasks' : 'project = MAPS AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, "Technical task")) AND parent in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+      }
+    },
     'NDA' : {
       'name' : 'Spotlight',
       'url' : 'https://jira3.inside.nsn.com',
@@ -28,17 +38,17 @@ settings = {
         'stories' : 'project = "RAN Dim" and issuetype = "User Story" and "Epic Link" in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
         'tasks' : 'project = "RAN Dim" AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, "Technical task")) AND parent in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
       }
-    },
-    'CSCLOUD': {
-      'name' : 'Cloud Controller',
-      'url' : 'https://jira3.inside.nsn.com',
-      'queries' : {
-        'project' : 'CSCLOUD',
-        'epics' : 'project = CSCLOUD AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
-        'stories' : 'project = CSCLOUD and issuetype = Story and "Epic Link" in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
-        'tasks' : 'project = CSCLOUD AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, "Technical task")) AND parent in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
-      }
-    }
+    }#,
+    # 'CSCLOUD': {
+    #   'name' : 'Cloud Controller',
+    #   'url' : 'https://jira3.inside.nsn.com',
+    #   'queries' : {
+    #     'project' : 'CSCLOUD',
+    #     'epics' : 'project = CSCLOUD AND issuetype = Epic AND status != Closed AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+    #     'stories' : 'project = CSCLOUD and issuetype = Story and "Epic Link" in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC',
+    #     'tasks' : 'project = CSCLOUD AND (issuetype in (subTaskIssueTypes(), Bug, Improvement, "Technical task")) AND parent in (%s) AND (labels != nobacklog OR labels is empty) ORDER BY Rank ASC'
+    #   }
+    # }
   }
 
 
